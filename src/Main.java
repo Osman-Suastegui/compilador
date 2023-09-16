e9d62d5d8176448e6e6f6a06672b4e25b2e4fa61
Osman <osman_suastegui@hotmail.com>
2023-09-15 18:56:17 -0700

    Se refactorizo el codigo

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        LEEMOS EL CONTENIDO DEL ARCHIVO
-        File file = new File("C:\\Users\\osman\\OneDrive\\Desktop\\COMPILADOR_JAVA\\src\\archivo.txt");
+        List<Contenido> contenido = leerContenidoDelArchivo("C:\\Users\\osman\\OneDrive\\Desktop\\COMPILADOR_JAVA\\src\\archivo.txt");
+
+        LinkList listaEnlazada = new LinkList();
+        AutomataIdentificadores AI = new AutomataIdentificadores(contenido, listaEnlazada);
+        AutomataNumeros AN = new AutomataNumeros(contenido, listaEnlazada);
+
+        procesarContenido(contenido, AI, AN, listaEnlazada);
+
+        System.out.println("LISTA ENLAZADA: ");
+        listaEnlazada.imprimir();
+    }
+
+    private static List<Contenido> leerContenidoDelArchivo(String filePath) throws IOException {
         List<Contenido> contenido = new ArrayList<>();
+        File file = new File(filePath);
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         int c;
         int renglon = 1;
-        int columna = 2;
+        int columna = 1;
 
         while ((c = br.read()) != -1) {
             if (c == 10) {
@@ -24,36 +36,28 @@ public class Main {
                 columna = 1;
             }
             if (c == 13) {
-              continue;
+                continue;
             }
             contenido.add(new Contenido((char) c, renglon, columna));
             columna++;
         }
         br.close();
-    //        TERMINAMOS DE LEER EL CONTENIDO DEL ARCHIVO
+        return contenido;
+    }
+
+    private static void procesarContenido(List<Contenido> contenido, AutomataIdentificadores AI, AutomataNumeros AN, LinkList listaEnlazada) throws Exception {
         int currIndx = 0;
 
-//        CREAMOS LOS AUTOMATAS PARA IDENTIFICADORES Y NUMEROS
-        LinkList listaEnlazada = new LinkList();
-        AutomataIdentificadores automataIdentificadores = new AutomataIdentificadores(contenido, listaEnlazada);
-        AutomataNumeros automataNumeros = new AutomataNumeros(contenido,listaEnlazada);
-
-//   LEE EL CONTENIDO DEL ARCHIVO Y LO MANDA A LOS AUTOMATAS PARA QUE CREEN LOS TOKENS
         while (currIndx < contenido.size()) {
             int car = contenido.get(currIndx).getCaracter();
 
             if (Character.isLetter(car)) {
-                currIndx = automataIdentificadores.validar(currIndx);
-            }
-            else if(Character.isDigit(car)){
-                currIndx = automataNumeros.validar(currIndx);
-            }
-            else{
+                currIndx = AI.validar(currIndx);
+            } else if (Character.isDigit(car)) {
+                currIndx = AN.validar(currIndx);
+            } else {
                 currIndx++;
             }
         }
-        System.out.println("LISTA ENLAZADA: ");
-        listaEnlazada.imprimir();
-
     }
 }
